79e94606aab30b5123cad6b23325cb56f2591d3d
Jefferson Griebel <jg.awsprod@gmail.com>
2025-11-16 17:34:59 -0500

    initial commit

---
+++ scores.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const getElement = selector => document.querySelector(selector);
+
+const displayScores = scores => {   
+    // filter scores
+
+
+    // sort filtered scores
+
+
+    // get total of filtered scores and build display string
+
+
+    // calculate the average 
+  
+
+    // display 
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    getElement("#add_score").addEventListener("click", () => {   
+                
+    });
+    
+    getElement("#clear_scores").addEventListener("click", () => {
+        
+    });
+
+    getElement("#sort").addEventListener("change", () => {
+        
+    });
+    
+    getElement("#filter").addEventListener("change", () => {
+        
+    });
+
+    // set focus on first text box on load
+    getElement("#first_name").focus();
+});
